b1dad1c2d8b241c9528001338048c724c5ee18b6
roshanchandna <rcwin2007@gmail.com>
2016-12-22 20:31:55 -0800

    Add background image
    This will actually add the background image. It makes it a little messy-looking though so see what you think.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<title>FUCK THE JETS</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body style="text-align:center;"> <h1 style="font-size:180px;font-family:Helvetica,Arial,sans-serif;">FUCK THE JETS</h1>
+<body background="img/FTJ.jpg" style="text-align:center;"> <h1 style="font-size:180px;font-family:Helvetica,Arial,sans-serif;">FUCK THE JETS</h1>
 <iframe width="640" height="360" src="https://www.youtube.com/embed/rAp57G1hLn0?autoplay=1" frameborder="0" allowfullscreen></iframe>
 	
 </body>
